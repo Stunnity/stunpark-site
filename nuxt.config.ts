364c79fed7714a68b95e83096b32afa33585f315
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-06-16 14:45:59 +0200

    feat: packages upgraded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    experimental: {
+        typedPages: true,
+    },
+
     runtimeConfig: {
         public: {
             siteUrl: process.env.NUXT_PUBLIC_SITE_URL || "https://stunpark.com",
         },
     },
+
     css: ["@/assets/css/global.css"],
+
     app: {
         head: {
             charset: "utf-16",
             viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
             title: "Stunpark Mobile App | Hassle-Free Parking Experience",
             meta: [
-                // <meta name="description" content="My amazing site">
-
                 {
                     name: "description",
                     content:
@@ -30,16 +34,24 @@ export default defineNuxtConfig({
             },
         },
     },
+
     components: [
         {
             path: "~~/components",
             pathPrefix: false,
         },
     ],
+
     plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
     image: {},
+
     routeRules: {
         "/*": { headers: { "Cache-Control": "max-age=31536000" } },
     },
+
     modules: ["@nuxtjs/tailwindcss", "vite-plugin-vue-type-imports/nuxt", "@nuxt/image-edge", "nuxt-og-image", "@nuxtjs/robots"],
+
+    devtools: {
+        enabled: true,
+    },
 })
