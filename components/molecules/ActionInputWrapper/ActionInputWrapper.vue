d0b921935f01c57a3529a98253b6fda736fe3b0c
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-06-19 17:18:44 +0200

    feat: responsive input

--- components/molecules/ActionInputWrapper/ActionInputWrapper.vue
+++ components/molecules/ActionInputWrapper/ActionInputWrapper.vue
@@ -7,7 +7,7 @@ function submit(e: Event) {
 }
 </script>
 <template>
-    <form class="flex border-2 border-white rounded-full p-2 items-center w-[30rem]" @submit="submit">
+    <form class="flex border-2 border-white rounded-full p-2 items-center md:w-[30rem]" @submit="submit">
         <div class="pl-4 pr-2 flex-1"><slot name="input" /></div>
         <div><slot name="action" /></div>
     </form>
